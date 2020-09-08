a1be41865a6609d34733fabfe1c317040e226d1c
Alfonso <fonsogms@gmail.com>
2020-09-08 13:12:25 +0200

    url working

--- src/config/typerorm.config.ts
+++ src/config/typerorm.config.ts
@@ -7,20 +7,10 @@ export const typeOrmConfig = (): TypeOrmModuleOptions => {
     return {
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      /*   host: 'ec2-52-30-161-203.eu-west-1.compute.amazonaws.com',
-      database: 'd7go4np2upmo0u',
-      username: 'ptvdcukdnbrbyd',
-      password:
-        '339856c1cd0e584e15cc3a2ddb7d43229a127183631d93ffae6563116f3951b3',
-      port: 5432,
- */
       entities: [__dirname + '/../**/*.entity.js'],
       synchronize: true,
     };
   } else {
-    // gets your default configuration
-    // you could get a specific config by name getConnectionOptions('production')
-    // or getConnectionOptions(process.env.NODE_ENV)
     return {
       type: 'postgres',
       host: 'localhost',
