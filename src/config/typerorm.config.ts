f712c2974e3ff0ae62d2ae9803f4bdb678df4912
Alfonso <fonsogms@gmail.com>
2020-09-08 13:09:21 +0200

    probando url

--- src/config/typerorm.config.ts
+++ src/config/typerorm.config.ts
@@ -5,16 +5,15 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig = (): TypeOrmModuleOptions => {
   if (process.env.DATABASE_URL) {
     return {
-      /*  url:
-        'postgres://ptvdcukdnbrbyd:339856c1cd0e584e15cc3a2ddb7d43229a127183631d93ffae6563116f3951b3@ec2-52-30-161-203.eu-west-1.compute.amazonaws.com:5432/d7go4np2upmo0u', */
       type: 'postgres',
-      host: 'ec2-52-30-161-203.eu-west-1.compute.amazonaws.com',
+      url: process.env.DATABASE_URL,
+      /*   host: 'ec2-52-30-161-203.eu-west-1.compute.amazonaws.com',
       database: 'd7go4np2upmo0u',
       username: 'ptvdcukdnbrbyd',
       password:
         '339856c1cd0e584e15cc3a2ddb7d43229a127183631d93ffae6563116f3951b3',
       port: 5432,
-
+ */
       entities: [__dirname + '/../**/*.entity.js'],
       synchronize: true,
     };
