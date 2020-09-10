ab541cc79e60b81dee0760b1b5558fd2c252afae
Alfonso <fonsogms@gmail.com>
2020-09-10 09:04:43 +0200

    fixing env bug

--- src/config/typerorm.config.ts
+++ src/config/typerorm.config.ts
@@ -3,23 +3,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig = (): TypeOrmModuleOptions => {
+  console.log(process.env);
   if (process.env.DATABASE_URL) {
+    console.log('working?');
+    console.log(process.env);
     return {
       type: 'postgres',
       url: process.env.DATABASE_URL,
       entities: [__dirname + '/../**/*.entity.js'],
       synchronize: true,
     };
-  } else {
-    return {
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'tito-1992',
-      database: 'foodsharer',
-      entities: [__dirname + '/../**/*.entity.js'],
-      synchronize: true,
-    };
   }
+  // else {
+  //   return {
+  //     type: 'postgres',
+  //     host: 'localhost',
+  //     port: 5433,
+  //     username: 'postgres',
+  //     password: 'tito-1992',
+  //     database: 'foodsharer',
+  //     entities: [__dirname + '/../**/*.entity.js'],
+  //     synchronize: true,
+  //   };
+  // }
 };
