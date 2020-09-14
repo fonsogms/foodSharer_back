febfb8ed565456d8872b0741ebc49706c2081f82
Alfonso <fonsogms@gmail.com>
2020-09-14 10:00:59 +0200

    cloudinary bug fix

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthGuard } from '@nestjs/passport';
 @Controller('upload')
-@UseGuards(AuthGuard())
 export class UploadController {
   @Post('/image')
   uploadImage(@Req() req: Request) {
